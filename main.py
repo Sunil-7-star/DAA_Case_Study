6484fa662051ae494fbf6000536239a9c16b6a8b
nageswar12345 <nageswardarisa@gmail.com>
2026-02-13 23:19:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from start_menu import StartMenu
 
 if __name__ == "__main__":
     root = tk.Tk()
-    StartMenu(root)
+    root.configure(bg="#1a1a2e")
+    app=StartMenu(root)
     root.mainloop()
