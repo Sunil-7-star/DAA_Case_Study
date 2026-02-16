e62b6fd672415caf1fe074bfa807f8e3894dae26
nageswar12345 <nageswardarisa@gmail.com>
2026-02-16 18:35:37 +0530

    Update ai_engine_dp.py

--- ai_engine_dp.py
+++ ai_engine_dp.py
@@ -1,1 +1,24 @@
-
+WHITE="white"
+BLACK="black"
+class DPAI:
+  
+  def is_winning_position(self):
+        key = self.get_state_key()
+        if key in self.memo:
+            return self.memo[key]
+        moves = self.get_valid_moves()
+        if len(moves) == 0:
+            self.memo[key] = False
+            return False
+        i = 0
+        while i < len(moves):
+            r, c = moves[i]
+            self.board[r][c] = BLACK
+            opponent_can_win = self.is_winning_position()
+            self.board[r][c] = WHITE
+            if opponent_can_win == False:
+                self.memo[key] = True
+                return True
+            i += 1
+        self.memo[key] = False
+        return False
