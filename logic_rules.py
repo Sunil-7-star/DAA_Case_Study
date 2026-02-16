9d8a291cc0dc771aa04a3ca1cfeb055e705844dc
Semanth-7963 <parvatanenisemanth@gmail.com>
2026-02-16 23:16:10 +0530

    Add adjacency check for black cells in RuleEngine

--- logic_rules.py
+++ logic_rules.py
@@ -1,8 +1,9 @@
-from tkinter import messagebox
 from collections import deque
 import copy
+
 WHITE = "white"
 BLACK = "black"
+
 class RuleEngine:
     def save_state(self):
         self.undo_stack.append(copy.deepcopy(self.board))
@@ -33,16 +34,39 @@ class RuleEngine:
                 col_count += 1
         return row_count > 1 or col_count > 1
 
+    def has_adjacent_black(self, r, c):
+        """Check if making this cell black would create adjacent black cells"""
+        # Check all 4 adjacent cells (up, down, left, right)
+        # Note: diagonal adjacency is NOT checked, only horizontal and vertical
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]  # right, left, down, up
+        
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            # Check if the adjacent cell is within bounds and is black
+            if 0 <= nr < self.grid_size and 0 <= nc < self.grid_size:
+                if self.board[nr][nc] == BLACK:
+                    return True
+        return False
+
     def valid_black(self, r, c):
+        """Check if a cell can be validly marked as black"""
+        # First check: No adjacent black cells
+        if self.has_adjacent_black(r, c):
+            return False
+        
+        # Second check: White cells must stay connected
         self.board[r][c] = BLACK
         ok = self.white_connected()
         self.board[r][c] = WHITE
         return ok
 
     def white_connected(self):
+        """Check if all white cells form a single connected group using BFS"""
         vis = [[False] * self.grid_size for _ in range(self.grid_size)]
         q = deque()
         start_found = False
+        
+        # Find the first white cell to start BFS
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 if self.board[i][j] != BLACK:
@@ -51,23 +75,32 @@ class RuleEngine:
                     start_found = True
                     break
             if start_found: break
-        if not start_found: return True
+        
+        if not start_found: return True  # All cells are black
+        
         cnt = 1
+        # BFS to count connected white cells
         while q:
             x, y = q.popleft()
-            for dx, dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+            # Check 4 adjacent cells (not diagonals)
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 nx, ny = x + dx, y + dy
                 if 0 <= nx < self.grid_size and 0 <= ny < self.grid_size:
                     if not vis[nx][ny] and self.board[nx][ny] != BLACK:
                         vis[nx][ny] = True
                         q.append((nx, ny))
                         cnt += 1
+        
+        # Count total white cells
         total_white = 0
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 if self.board[i][j] != BLACK:
                     total_white += 1
+        
+        # All white cells must be connected
         return cnt == total_white
+
     def refresh(self):
         for r in range(self.grid_size):
             for c in range(self.grid_size):
